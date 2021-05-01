5fa012b0e0de9bdedb966886f1023caf8c24b6be
Matthew Newman <theMatthewNewman@gmail.com>
2021-05-01 15:29:33 -0400

    edits to themeing

---
+++ elpa/zerodark-theme-20210216.1640/zerodark-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "zerodark-theme" "20210216.1640" "A dark, medium contrast theme for Emacs" '((all-the-icons "2.0.0")) :commit "ee49ea9e875d7a3da63386880ca3a9e10b1051e5" :authors '(("Nicolas Petton" . "nicolas@petton.fr")) :maintainer '("Nicolas Petton" . "nicolas@petton.fr") :keywords '("themes") :url "https://github.com/NicolasPetton/zerodark-theme")
--- init.el
+++ init.el
@@ -42,7 +42,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (use-package))))
+ '(package-selected-packages (quote (diminish zerodark-theme fancy-battery use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
